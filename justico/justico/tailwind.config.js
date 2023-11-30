fa01631febec8926e967d028fe22cea39aab0a6c
corder2233 <99630613+corder2233@users.noreply.github.com>
2023-12-01 00:44:17 +0530

    final upload
    this is the 1str version of my code

---
+++ justico/justico/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      Inclusive: ["Inclusive Sans", "sans-serif"],
+      Poppins: ["Poppins", "sans-serif"],
+    }
+  },
+  plugins: [],
+}
+
